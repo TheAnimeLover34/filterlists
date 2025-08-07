f4584fb764f8cfda99c9d06d41362e1998de52ba
Parsa307 <79400634+Parsa307@users.noreply.github.com>
2025-08-07 08:11:11 +0330

    Add a Smart Version. To make it harder for those Soft98 Ninjas to Patch it.

---
+++ UserScripts/Soft98_Smart_Download_Redirect.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Soft98 Smart Download Redirect
+// @namespace    Violentmonkey Scripts
+// @version      1.0
+// @match        https://soft98.ir/*
+// @grant        none
+// @description  Redirect to download link if it points to dl*.soft98.ir, bypassing AdBlock/VPN warning.
+// @downloadURL  https://raw.githubusercontent.com/Parsa307/filterlists/main/UserScripts/Soft98_Smart_Download_Redirect.user.js
+// @updateURL    https://raw.githubusercontent.com/Parsa307/filterlists/main/UserScripts/Soft98_Smart_Download_Redirect.user.js
+// @homepageURL  https://github.com/Parsa307/filterlists
+// @supportURL   https://github.com/Parsa307/filterlists/issues
+// @author       Parsa307
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    document.body.addEventListener('click', function(event) {
+        let el = event.target;
+
+        // Climb up the DOM to find a link
+        while (el && el !== document.body) {
+            if (el.tagName === 'A' && el.href) {
+                try {
+                    const url = new URL(el.href);
+                    // Only redirect if hostname starts with "dl" and is under soft98.ir
+                    if (/^dl\d*\.soft98\.ir$/.test(url.hostname)) {
+                        window.location.href = el.href;
+                        break;
+                    }
+                } catch (e) {
+                    // not a valid URL, ignore
+                }
+            }
+            el = el.parentElement;
+        }
+    }, { capture: true, passive: false });
+})();
